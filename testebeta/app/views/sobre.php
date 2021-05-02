11883a446cf7d7f60067ed305995550a379dc55c
moisesnmoraes <47926177+moisesnmoraes@users.noreply.github.com>
2021-05-01 22:45:34 -0300

    Add files via upload

---
+++ testebeta/app/views/sobre.php
@@ -0,0 +1,22 @@
+<div id="main" class="center">
+	<span style="color:#9bbd46">- SOBRE -</span>
+	<h2>Ut placerat enim vitae metus gravida sollicitudin.</h2>
+</div>
+
+<div id="sobre">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<img src="app/img/j4.jpg" class="img-thumbnail img-responsive center" >
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempor nisi tellus, a dictum risus sagittis vitae. Mauris rhoncus, velit quis suscipit efficitur, arcu dolor porta felis, et tincidunt lacus urna a dolor. Nunc faucibus, nibh eget pulvinar tempus, odio sem fermentum justo, at luctus risus leo non turpis. Praesent est velit, feugiat nec arcu condimentum, vestibulum varius ligula. Aliquam at nulla in turpis feugiat mattis nec a nisi. Sed rutrum sed lectus sed tincidunt. Nulla venenatis tellus non risus gravida aliquam. Curabitur at molestie ligula. Mauris ac libero in est scelerisque luctus nec in mi. Pellentesque a finibus neque, ac rhoncus dolor. Mauris placerat fringilla lacus eu tempor. Morbi pulvinar elementum euismod.
+
+</p>
+				<p>Ut elementum tempus libero nec tincidunt. Sed sollicitudin tortor tortor, ut hendrerit metus lobortis vitae. Integer faucibus tortor eu pharetra cursus. Maecenas pharetra ante metus, a iaculis tortor consequat et. Vestibulum vestibulum, velit sed luctus dictum, tortor ipsum ultrices ante, a sodales ante odio ac turpis. Proin accumsan lacus mollis venenatis pretium. Sed imperdiet nibh at erat cursus consequat. Cras sit amet leo vel augue cursus rutrum.
+
+</p>
+				<p>Vivamus tempus tortor nec ex cursus, sed interdum urna varius. Quisque blandit neque sapien, non fermentum odio suscipit ac. Sed at elementum erat. Donec a est eget enim luctus sollicitudin. Mauris neque eros, eleifend non purus nec, commodo egestas ante. Nulla cursus lacus ac hendrerit laoreet. Proin ante dolor, elementum vitae elit eu, posuere vehicula ligula. Suspendisse sed urna id felis luctus varius. Donec accumsan hendrerit tellus a auctor. Donec facilisis ligula eget nunc egestas ornare. Nunc malesuada velit eget dictum tincidunt.</p>
+				<p style="text-align:center"><a href="?i=navegar" class="btn btn-primary btn-lg">Conheça todas as sequências</a></p>
+			</div>
+		</div>
+	</div>
+</div>
